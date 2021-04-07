3933ed3082e71a2d9a9934a918de97706c3c50b6
Jerson Brito <jersonlbrito@hotmail.com>
2021-04-07 18:41:48 -0300

    ajustando referencia

---
+++ src/PdfGenerator/GeneratePdf.cs
@@ -0,0 +1,45 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+
+namespace PdfGenerator
+{
+    public class GeneratePdf
+    {
+        public Uri GitHubAvatar { get; }
+
+        public GeneratePdf(Uri urlGitHub)
+        {
+            GitHubAvatar = urlGitHub;
+        }
+
+        public MemoryStream PdfInMemoryStream => Generate();
+
+        private MemoryStream Generate()
+        {
+            try
+            {
+                var gitHubAvata = Image.GetInstance(GitHubAvatar);
+
+                var ms = new MemoryStream();
+                var doc = new Document(new Rectangle(urx: gitHubAvata.Width, ury: gitHubAvata.Height));
+                var pdf = new PdfHelper(doc, ms);
+
+                pdf.AddImage(gitHubAvata, absoluteX: 0, absoluteY: 0);
+                pdf.Text("teste", sizeFont: 20, positionX: 20, positionY: 60, nameFont: BaseFont.HELVETICA_BOLD, align: Element.ALIGN_LEFT, color: BaseColor.White);
+
+                doc.Close();
+                var msInfo = ms.ToArray();
+                ms.Write(msInfo, 0, msInfo.Length);
+
+                ms.Position = 0;
+                return ms;
+            }
+            catch(InvalidOperationException)
+            {
+                throw new InvalidOperationException($"algo de errado com o link {GitHubAvatar.AbsoluteUri.Replace(".png", "")}");
+            }
+        }
+    }
+}
---
+++ src/PdfGenerator/PdfHelper.cs
@@ -0,0 +1,100 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace PdfGenerator
+{
+    public class PdfHelper
+    {
+        #region Properties
+
+        private readonly Document _doc;
+        private readonly PdfContentByte _contentByte;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE0044:Adicionar modificador somente leitura", Justification = "Não ajuda")]
+        private PdfWriter pdf;
+
+        internal float CenterX
+           => _doc.PageSize.Width / 2;
+
+        internal float CenterY
+           => _doc.PageSize.Height / 2;
+
+        #endregion Properties
+
+        internal PdfHelper(Document doc, MemoryStream ms)
+        {
+            _doc = doc;
+            pdf = PdfWriter.GetInstance(doc, ms);
+            pdf.CloseStream = false;
+            _doc.Open();
+            _contentByte = pdf.DirectContent;
+        }
+
+        #region Text
+
+        private void Text(string text, string nameFont, int sizeFont, float positionX, float positionY, BaseColor color, int align)
+        {
+            var font = BaseFont.CreateFont(nameFont, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            _contentByte.SetColorFill(color);
+            _contentByte.SetFontAndSize(font, sizeFont);
+            _contentByte.BeginText();
+            _contentByte.ShowTextAligned(align, text, positionX, positionY, 0);
+            _contentByte.EndText();
+        }
+
+        internal void Text(string text, string nameFont = null, int? sizeFont = null, float? positionX = null, float? positionY = null, BaseColor color = null, int? align = null)
+             => Text
+                 (
+                     text
+                     , nameFont ?? BaseFont.HELVETICA
+                     , sizeFont ?? 12
+                     , positionX ?? CenterX
+                     , positionY ?? CenterY
+                     , color ?? BaseColor.Black
+                     , align ?? Element.ALIGN_CENTER
+                  );
+
+        #endregion Text
+
+        #region Image
+
+        private void AddImage(Image image, float absoluteX, float absoluteY, float width, float height)
+        {
+            image.ScaleToFit(width, height);
+            image.SetAbsolutePosition(absoluteX, absoluteY);
+
+            _doc.Add(image);
+        }
+
+        internal void AddImage(Image image, float? absoluteX = null, float? absoluteY = null, float? width = null, float? height = null)
+            => AddImage
+            (
+                image
+                , absoluteX ?? CenterX
+                , absoluteY ?? CenterY
+                , width ?? image.Width
+                , height ?? image.Height
+            );
+
+        #endregion Image
+
+        #region Rectangle
+
+        internal void Rectangle(float width, float height, float? xInit = null, float? yInit = null, float? widthLine = null, float? radius = null, BaseColor boaderColor = null, BaseColor internColor = null, float? opacity = null)
+        {
+            var gs1 = new PdfGState
+            {
+                FillOpacity = opacity ?? 1,
+            };
+            _contentByte.SetGState(gs1);
+            _contentByte.SaveState();
+            _contentByte.Rectangle(xInit ?? 0, yInit ?? 0, width, height);
+            _contentByte.SetColorFill(internColor ?? BaseColor.White);
+            _contentByte.Fill();
+            _contentByte.RestoreState();
+        }
+
+        #endregion Rectangle
+    }
+}
