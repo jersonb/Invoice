001e1be730284bf724e5680ebd97f0808bdc6084
Jerson Brito <jersonlbrito@hotmail.com>
2021-04-07 10:42:55 -0300

    first commit

---
+++ src/Invoice.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Invoice.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
