de895b96ec34a2a7f7b8932b24a6c9086c086ade
jersonb <jersonlbrito@hotmail.com>
2021-04-15 13:32:10 -0300

    alterando telefone

--- src/PdfGenerator/Labels.cs
+++ src/PdfGenerator/Labels.cs
@@ -6,7 +6,7 @@
         public const string LEGAL_NUMBER = "11.117.014/0001-06";
         public const string LEGAL_REGIONAL_NUMBER = "6124836";
         public const string ADDRESS = "RUA RIO OCEANICO, SALA 08, 422, IMBIRIBEIRA, RECIFE, PE";
-        public const string PHONE = "81-99540-2828";
+        public const string PHONE = "81-99540-3838";
         public const string CEP = "52.200-050";
         public const string DESCRIPTION = "Nota Fiscal de Serviço - FATURA";
         public const string LEGAL_DESCRIPTION = "NÃO INCIDÊNCIA DE ISS CONF. LEI COMP N° 166 21/07/03 DOU 01/08/03";
