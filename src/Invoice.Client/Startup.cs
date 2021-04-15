c132216dc0bb984de4ce8c89bdc56029845953cf
jersonb <jersonlbrito@hotmail.com>
2021-04-15 01:39:22 -0300

    adicionando cultura default

--- src/Invoice.Client/Startup.cs
+++ src/Invoice.Client/Startup.cs
@@ -1,6 +1,8 @@
+using System.Globalization;
 using Invoice.Client.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,14 @@ namespace Invoice.Client
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationData dataContext)
         {
             dataContext.Database.Migrate();
+
+ var supportedCultures = new[] { new CultureInfo("pt-BR") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             if(env.IsDevelopment())
             {
